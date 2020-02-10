28a40304a00841cedaf9462186e2c90fb96d6913
hofey <hofey@live.com>
2020-02-10 11:40:57 +0800

    一次修改

--- src/main/java/com/hofey/dwy/DwyApplication.java
+++ src/main/java/com/hofey/dwy/DwyApplication.java
@@ -2,8 +2,6 @@ package com.hofey.dwy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class DwyApplication {
 

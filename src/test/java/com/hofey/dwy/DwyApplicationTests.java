1040806a673cd8ecb85634989820988049732e6a
hofey <hofey@live.com>
2020-04-08 10:35:00 +0800

    java时间格式化分析

--- src/test/java/com/hofey/dwy/DwyApplicationTests.java
+++ src/test/java/com/hofey/dwy/DwyApplicationTests.java
@@ -3,11 +3,28 @@ package com.hofey.dwy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+
 @SpringBootTest
 class DwyApplicationTests {
 
     @Test
     void contextLoads() {
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ISO_DATE_TIME;//2020-04-08T10:21:34.818
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("yyyyMMddHHMMSS");
+
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.BASIC_ISO_DATE;//20200408
+        DateTimeFormatter dateTimeFormatter4 = DateTimeFormatter.ISO_LOCAL_TIME;//10:31:31.142
+        DateTimeFormatter dateTimeFormatter5 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;//2020-04-08T10:32:44.436
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime.format(dateTimeFormatter5));
+
+
+
     }
 
 }
